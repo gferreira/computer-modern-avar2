9324280a808a47f892abbd74cbf496d48a914698
gferreira <gustavo@hipertipo.com>
2026-03-06 06:58:35 -0300

    adding controller.py (WIP)

---
+++ Tools/controller.py
@@ -0,0 +1,44 @@
+from importlib import reload
+import xTools4.modules.xproject
+reload(xTools4.modules.xproject)
+
+import os
+from xTools4.modules.xproject import xProject
+
+
+class ComputerModernController(xProject):
+    
+    def __init__(self, folder, familyName, subFamily):
+        self.baseFolder = folder
+        self.familyName = familyName
+        self.subFamily  = subFamily
+
+    @property
+    def designspaceFile(self):
+        return f"{self.familyName.replace(' ', '')}-{self.subFamily.replace(' ', '')}.designspace"
+
+    @property
+    def sourcesFolder(self):
+        return os.path.join(self.baseFolder, self.sourcesFolderName, self.subFamily)
+
+    @property
+    def defaultSourcePath(self):
+        return os.path.join(self.sourcesFolder, f"{self.familyName.replace(' ', '')}-{self.subFamily.replace(' ', '')}_{self.defaultName}.ufo")
+
+
+if __name__ == '__main__':
+
+    folder = os.path.dirname(os.getcwd())
+
+    subFamily = ['Roman', 'Italic', 'Sans', 'Mono'][1]
+
+    p = ComputerModernController(folder, 'Computer Modern', subFamily)
+    p.printSettings()
+
+    # initialize designspace
+    p.parametricAxes = [
+        'XOPQ', 'YOPQ', 'XTRA', 'XTSP', 'XSHA', 'YSHA',
+    ]
+    p.initializeParametricSources()
+    p.setSourceNamesFromMeasurements()
+
