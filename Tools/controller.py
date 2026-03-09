ff148a47bc8bae07bdbc620f9c735de32764601e
gferreira <gustavo@hipertipo.com>
2026-03-09 18:51:38 -0300

    cleaning up Roman default source, editing measurements, more work on controller.py

--- Tools/controller.py
+++ Tools/controller.py
@@ -3,7 +3,7 @@ import xTools4.modules.xproject
 reload(xTools4.modules.xproject)
 
 import os
-from xTools4.modules.xproject import xProject
+from xTools4.modules.xproject import xProject, DesignSpaceDocument
 
 
 class ComputerModernController(xProject):
@@ -15,7 +15,7 @@ class ComputerModernController(xProject):
 
     @property
     def designspaceFile(self):
-        return f"{self.familyName.replace(' ', '')}-{self.subFamily.replace(' ', '')}.designspace"
+        return f"{self.subFamily.replace(' ', '')}.designspace"
 
     @property
     def sourcesFolder(self):
@@ -23,22 +23,40 @@ class ComputerModernController(xProject):
 
     @property
     def defaultSourcePath(self):
-        return os.path.join(self.sourcesFolder, f"{self.familyName.replace(' ', '')}-{self.subFamily.replace(' ', '')}_{self.defaultName}.ufo")
+        return os.path.join(self.sourcesFolder, f"{self.subFamily.replace(' ', '')}_{self.defaultName}.ufo")
+
+    @property
+    def varFontFile(self):
+        fileName = f"{self.familyName}-{self.subFamily}.ttf"
+        return fileName.replace(' ', '')
+
+    def buildDesignspace(self, tuning=False, instances=False):
+
+        self.designspace = DesignSpaceDocument()
+        # self.addParametricAxes()
+        self.addDefaultSource()
+        # self.addParametricSources()
+        self.addCustomKeysToLib()
+        self.save()
 
 
 if __name__ == '__main__':
 
     folder = os.path.dirname(os.getcwd())
 
-    subFamily = ['Roman', 'Italic', 'Sans', 'Mono'][1]
+    subFamily = ['Roman', 'Italic', 'Sans', 'Mono'][0]
 
     p = ComputerModernController(folder, 'Computer Modern', subFamily)
-    p.printSettings()
+    # p.printSettings()
 
-    # initialize designspace
-    p.parametricAxes = [
-        'XOPQ', 'YOPQ', 'XTRA', 'XTSP', 'XSHA', 'YSHA',
+    parametricAxes = [
+        'XOPQ',
+        'XTRA',
+        # 'YOPQ',
+        # etc.
     ]
-    p.initializeParametricSources()
-    p.setSourceNamesFromMeasurements()
+    p.createParametricSources(parametricAxes)
 
+    p.buildDesignspace()
+
+    # p.setSourceNamesFromMeasurements()
