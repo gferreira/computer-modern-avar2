31c5978e9cc978dc9cb4f2b45c2de138fb5a8a8c
gferreira <gustavo@hipertipo.com>
2026-03-09 19:14:20 -0300

    adding XOPQ sources as example, adding script to extract blends from reference fonts

--- Tools/controller.py
+++ Tools/controller.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 
     parametricAxes = [
         'XOPQ',
-        'XTRA',
+        # 'XTRA',
         # 'YOPQ',
         # etc.
     ]
---
+++ Tools/reference/extract-measurements.py
@@ -0,0 +1,49 @@
+# menuTitle: extract parametric blends from ComputerModernPS
+
+from importlib import reload
+import xTools4.modules.measurements
+reload(xTools4.modules.measurements)
+
+import os, glob, json
+from xTools4.modules.measurements import extractMeasurements
+
+subFamilyName    = ['Roman', 'Italic', 'Sans'][2]
+baseFolder       = os.path.dirname(os.path.dirname(os.getcwd()))
+sourcesFolder    = os.path.join(baseFolder, 'Sources', subFamilyName, 'reference')
+measurementsPath = os.path.join(sourcesFolder, 'measurements.json')
+blendsPath       = os.path.join(sourcesFolder, 'blends.json')
+parametricAxes   = 'XOUC XOLC XOFI YOUC YOLC YOFI XTUC XTLC XTFI YTUC YTLC YTAS YTDE YTFI XUCS XLCS XFIR XSHU YSHU XSHL YSHL XSVU YSVU'.split()
+
+assert os.path.exists(sourcesFolder)
+assert os.path.exists(measurementsPath)
+
+# define blended axes
+
+axes = {
+    "opsz" : {
+      "name"    : "Optical size",
+      "default" : 10,
+      "minimum" : 5,
+      "maximum" : 17,
+    },
+}
+
+# extract measurements from ComputerModernPS instances
+
+ufos = [f for f in glob.glob(f'{sourcesFolder}/*.ufo')]
+
+sources = extractMeasurements(ufos, measurementsPath, parametricAxes)
+
+# save measurements to JSON blends file
+
+blendsDict = {
+    'axes'    : axes,
+    'sources' : sources,
+}
+
+print('saving blended axes and measurements to blends.json...\n')
+
+with open(blendsPath, 'w', encoding='utf-8') as f:
+    json.dump(blendsDict, f, indent=2)
+
+print('...done!\n')
