e77a9bd9831dd21fd0af42b13acbd3a1b20c5632
gferreira <gustavo@hipertipo.com>
2026-03-30 11:13:42 -0300

    building avar2 font, adding html proof comparing avar2/reference fonts

--- build.sh
+++ build.sh
@@ -2,13 +2,17 @@
 
 echo "Building variable fonts..."
 
-echo "  Building Computer Modern Roman..."
+echo "  Building Computer Modern Roman (reference font)..."
 fontmake -m Sources/Roman/reference/Roman.designspace -o variable --output-path Fonts/ComputerModern-Roman.ttf  --feature-writer None --no-generate-GDEF --keep-direction --verbose WARNING
 
-# echo "  Building Computer Modern Italic..."
+# echo "  Building Computer Modern Italic (reference font)..."
 # fontmake -m Sources/Italic/Italic.designspace -o variable --output-path Fonts/ComputerModern-Italic.ttf  --feature-writer None --no-generate-GDEF --keep-direction --verbose WARNING
 
-# echo "  Building Computer Modern Sans..."
+# echo "  Building Computer Modern Sans (reference font)..."
 # fontmake -m Sources/Sans/Sans.designspace -o variable --output-path Fonts/ComputerModern-Sans.ttf  --feature-writer None --no-generate-GDEF --keep-direction --verbose WARNING
+
+echo "  Building Computer Modern Roman (avar2)..."
+fontmake -m Sources/Roman/Roman.designspace -o variable --output-path Fonts/ComputerModern-Roman_avar2.ttf  --feature-writer None --no-generate-GDEF --keep-direction --verbose WARNING
+
 
 echo "done!"
