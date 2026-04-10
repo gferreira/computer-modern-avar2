aaf0f5ad49385cb453c50c6ccbcb12a704e560b0
ibrahim <48119402+ibo-o@users.noreply.github.com>
2026-04-10 20:37:39 +0200

    Update controller.py

--- Tools/controller.py
+++ Tools/controller.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
 
     folder = os.path.dirname(os.getcwd())
 
-    subFamily = ['Roman', 'Italic', 'Sans', 'Mono'][1]
+    subFamily = ['Roman', 'Italic', 'Sans', 'Mono'][0]
 
     parametricAxes = {
         'Roman'  : ['XOPQ', 'XTRA', 'YOPQ', 'XTSP', 'XSHA', 'YSHA', 'XSVA', 'YSVA', 'YTUC', 'YTLC', 'YTAS', 'YTDE', 'BRKT', 'CUPS', 'WDSP'], # 'YTLC'
@@ -111,13 +111,13 @@ if __name__ == '__main__':
     p = ComputerModernController(folder, 'Computer Modern', subFamily)
 
     # p.printSettings()
-    # p.createParametricSources(['WDSP'], minSource=True, maxSource=True)
+    # p.createParametricSources(['XQAC', 'YQAC'], minSource=True, maxSource=True)
 
-    # p.cleanupSources(parametric=True, tuning=False)
-    # p.normalizeSources(parametric=True, tuning=False)
+    p.cleanupSources(parametric=True, tuning=False)
+    p.normalizeSources(parametric=True, tuning=False)
 
-    p.setSourceNamesFromMeasurements(preflight=True)
+    # p.setSourceNamesFromMeasurements(preflight=True)
     
-    p.parametricAxes = parametricAxes[subFamily]
-    p.parametricAxesHidden = False
-    p.buildDesignspace(patchBlends=False)
+    # p.parametricAxes = parametricAxes[subFamily]
+    # p.parametricAxesHidden = False
+    # p.buildDesignspace(patchBlends=False)
